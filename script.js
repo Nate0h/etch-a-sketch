9c8c532544fb8bb4f18337f7cdbc10a42d59d18c
Nathaniel Colon <nathanielcolon12@gmail.com>
2023-05-18 00:10:36 -0400

    added prompt functionality

--- script.js
+++ script.js
@@ -3,43 +3,48 @@ butt.textContent = "Click Me";
 document.body.appendChild(butt);
 
 butt.addEventListener('click', gridSize);*/
+
 const butt = document.querySelector("button");
 butt.addEventListener("click", gridSize);
 
 const container = document.querySelector("#container");
-
 function createGrid(x = 16){
-for(let i = 0; i < x; i++){
-let row = document.createElement("div");
-row.classList.add("row");
-//creates 16 flex rows 
-for(let i = 0; i < x; i++){
-//creates 16 squares and appends them all to the above row
-let square = document.createElement("square");
-square.classList.add("square");
-row.appendChild(square);
-}
-//attaches the row to the container 
-container.appendChild(row);
-}
-}
+    for(let i = 0; i < x; i++){
+    let row = document.createElement("div");
+    row.classList.add("row");
+    //creates 16 flex rows 
+    for(let i = 0; i < x; i++){
+    //creates 16 squares and appends them all to the above row
+    let square = document.createElement("square");
+    square.classList.add("square");
+    row.appendChild(square);
+    }
+    //attaches the row to the container 
+    container.appendChild(row);
+    }
+    }
+
 createGrid();
-
-//function to change color of square
 function hoverEffect(e){
-this.style.backgroundColor = 'yellow';
-}
-const keys = document.querySelectorAll('.square');
-keys.forEach(key => key.addEventListener('mouseover', hoverEffect));
-
+    this.style.backgroundColor = 'yellow';
+    }
+    
+    
 function gridSize(){
-let num = prompt("How square dimension do you want to grid to be?");
- num = parseInt(num);
-while(num > 100){
-num = prompt("How square dimension do you want to grid to be?");
-num = parseInt(num);
-content.remove();
-createGrid(num);
-}
-}
+    container.innerHTML = '';
+    let num = prompt("What square dimension do you want to grid to be?");
+     num = parseInt(num);
+    while(num > 100 ){
+    num = prompt("What square dimension do you want to grid to be?");
+    num = parseInt(num);
+    }
+    createGrid(num);
+    const keys = document.querySelectorAll('.square');
+    keys.forEach(key => key.addEventListener('mouseover', hoverEffect));
+    }
+
+
+    const keys = document.querySelectorAll('.square');
+    keys.forEach(key => key.addEventListener('mouseover', hoverEffect));
+
 
